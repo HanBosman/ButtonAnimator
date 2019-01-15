67079cc0c5481634e6d243e5d88a071946965f19
HanBosman <hanjrbosman@hotmail.com>
2019-01-16 00:21:36 +0100

    Basis structuur,

---
+++ main.js
@@ -0,0 +1,13 @@
+// zo ver ik het begrijp is dit een goede basis set up om te beginnen. 
+
+var Module = (function () {
+ var a = undefined;
+  return {
+    publicMethod: function () {
+      // code
+    }
+  };
+
+})();
+
+Module.publicMethod();
